ead08e9d67cd609dbd39d47b1fef5a0333ded5c5
Stefen Wakefield <me@xstefen.dev>
2021-09-13 06:32:10 -0500

    kebab: track some upstream system.prop changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,6 +36,7 @@ SOONG_CONFIG_ONEPLUS_KONA_FOD_SIZE = 174
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Partitions
